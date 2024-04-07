2afc2be29eb0b5bed7ee6d6c541390867ffb95aa
kurodoke <ariefsatrio098@gmail.com>
2024-04-07 23:48:45 +0700

    i forgot to stage this file :), this file was part of "implementing the model to packet"

--- core-network/connection.ts
+++ core-network/connection.ts
@@ -1,8 +1,12 @@
 import net from "net";
+import { Player } from "../model/player";
+import { Account } from "../model/account";
 
 class Connection{
-    sessionId: number;
-    socket: net.Socket;
+    public account: Account = null;
+    public player: Player = null;
+    declare public sessionId: number;
+    declare public socket: net.Socket;
 
     constructor(socket: net.Socket, sessionId: number) {
         this.socket = socket;
