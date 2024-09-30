1a0103d2c2c67fa426bc8fadb44b679a557aa341
arief satrio <ariefsatrio098@gmail.com>
2024-09-30 14:44:42 +0700

    feat: update readme md

--- README.md
+++ README.md
@@ -1,17 +1,27 @@
-
 # PointBlank Server V41
 
 Server PointBlank Turkey V41 Based on Node.js (ON WIP)
 
+## Configuration
+
+Open the folder:
+
+```bash
+data\file\config
+```
+
+You can change any settings you want. For example, the default database name is `nodepb`, but you can change it to `postgres` or any other name you prefer.
 
 ## Installation
+
+Make sure you have the latest Node.js version, at least v18.20.4.
 
 Install dependency used with npm
 
 ```bash
   npm install
 ```
-    
+
 Build the project with TypeScript
 
 ```bash
@@ -23,7 +33,7 @@ Run the server
 ```bash
   node ./dist/server.js
 ```
+
 ## Authors
 
-- [@kurodoke](https://www.github.com/kurodoke)
-
+-   [@kurodoke](https://www.github.com/kurodoke)
--- util/database.ts
+++ util/database.ts
@@ -20,7 +20,7 @@ import { AuthSettingServer } from "../config/authSettingServer";
 import { DatabaseSetting } from "../config/databaseSetting";
 import { Log } from "./log";
 
-interface IModel{
+interface IModel {
     account: AccountModelStatic;
     player: PlayerModelStatic;
     clan: ClanModelStatic;
@@ -35,25 +35,30 @@ interface IModel{
     message: MessageModelStatic;
 }
 
-class Database{
+class Database {
     private _connection: Sequelize;
     private static _instance: Database;
     public model: IModel;
 
-    constructor(){
-
-
+    constructor() {
         this._connection = new Sequelize({
             dialect: "postgres",
             host: DatabaseSetting.dbHost ? DatabaseSetting.dbHost : "localhost",
-            password: DatabaseSetting.dbPass ? DatabaseSetting.dbPass.toString() : "123456",
+            password: DatabaseSetting.dbPass
+                ? DatabaseSetting.dbPass.toString()
+                : "123456",
             port: DatabaseSetting.dbPort ? DatabaseSetting.dbPort : 5432,
-            database: DatabaseSetting.dbName ? DatabaseSetting.dbName : 'postgres',
-            username: DatabaseSetting.dbUser ? DatabaseSetting.dbUser : "postgres",
-            logging: AuthSettingServer.debugMode ? (msg) => Log.getLogger("database").info(msg) : false,
-        })
+            database: DatabaseSetting.dbName
+                ? DatabaseSetting.dbName
+                : "postgres",
+            username: DatabaseSetting.dbUser
+                ? DatabaseSetting.dbUser
+                : "postgres",
+            logging: AuthSettingServer.debugMode
+                ? (msg) => Log.getLogger("database").info(msg)
+                : false,
+        });
         // this._connection = new Sequelize('postgres://postgres:123456@localhost:5432/nodepb');
-        
 
         /**
          * model configuration
@@ -71,78 +76,109 @@ class Database{
             store: StoreModel(this._connection),
             coupon: CouponModel(this._connection),
             message: MessageModel(this._connection),
-        }
+        };
 
         /**
          * relation of the model
          */
-        this.model.account.hasOne(this.model.player, {foreignKey: "id"})
-        this.model.player.belongsTo(this.model.account, {foreignKey: "id"});
-        
-        this.model.player.hasOne(this.model.clan, {foreignKey: "owner"});
-        this.model.clan.belongsTo(this.model.player, {foreignKey: "owner"});
+        this.model.account.hasOne(this.model.player, { foreignKey: "id" });
+        this.model.player.belongsTo(this.model.account, { foreignKey: "id" });
 
-        this.model.player.hasOne(this.model.player_equipment, {foreignKey: "playerId"});
-        this.model.player_equipment.belongsTo(this.model.player, {foreignKey: "playerId"});
+        this.model.player.hasOne(this.model.clan, { foreignKey: "owner" });
+        this.model.clan.belongsTo(this.model.player, { foreignKey: "owner" });
 
-        this.model.player.hasOne(this.model.player_stat, {foreignKey: "playerId"});
-        this.model.player_stat.belongsTo(this.model.player, {foreignKey: "playerId"});
+        this.model.player.hasOne(this.model.player_equipment, {
+            foreignKey: "playerId",
+        });
+        this.model.player_equipment.belongsTo(this.model.player, {
+            foreignKey: "playerId",
+        });
 
-        this.model.player.hasOne(this.model.player_mission, {foreignKey: "playerId"});
-        this.model.player_mission.belongsTo(this.model.player, {foreignKey: "playerId"});
-        
-        this.model.player.hasOne(this.model.player_title, {foreignKey: "playerId"});
-        this.model.player_title.belongsTo(this.model.player, {foreignKey: "playerId"});
+        this.model.player.hasOne(this.model.player_stat, {
+            foreignKey: "playerId",
+        });
+        this.model.player_stat.belongsTo(this.model.player, {
+            foreignKey: "playerId",
+        });
+
+        this.model.player.hasOne(this.model.player_mission, {
+            foreignKey: "playerId",
+        });
+        this.model.player_mission.belongsTo(this.model.player, {
+            foreignKey: "playerId",
+        });
+
+        this.model.player.hasOne(this.model.player_title, {
+            foreignKey: "playerId",
+        });
+        this.model.player_title.belongsTo(this.model.player, {
+            foreignKey: "playerId",
+        });
 
         this.model.player.hasMany(this.model.player_inventory, {
             foreignKey: "playerId",
         });
 
-        this.model.player_inventory.belongsTo(this.model.player, {foreignKey: "playerId"});
-        this.model.player_inventory.belongsTo(this.model.item, {foreignKey: "itemId"});
+        this.model.player_inventory.belongsTo(this.model.player, {
+            foreignKey: "playerId",
+        });
+        this.model.player_inventory.belongsTo(this.model.item, {
+            foreignKey: "itemId",
+        });
 
         // this.model.item.hasMany(this.model.player_inventory, {
         //     foreignKey: "itemId",
         // });
 
-        this.model.item.hasMany(this.model.store, {foreignKey: "itemId"});
-        this.model.store.belongsTo(this.model.item, {foreignKey: "itemId"});
+        this.model.item.hasMany(this.model.store, { foreignKey: "itemId" });
+        this.model.store.belongsTo(this.model.item, { foreignKey: "itemId" });
 
-        this.model.player.hasOne(this.model.coupon, {foreignKey: "playerId"});
-        this.model.coupon.belongsTo(this.model.player, {foreignKey: "playerId"});
+        this.model.player.hasOne(this.model.coupon, { foreignKey: "playerId" });
+        this.model.coupon.belongsTo(this.model.player, {
+            foreignKey: "playerId",
+        });
 
         this.model.player.hasMany(this.model.message, {
             foreignKey: "senderId",
         });
 
-        this.model.message.belongsTo(this.model.player, {foreignKey: "senderId", as: "Sender"});
+        this.model.message.belongsTo(this.model.player, {
+            foreignKey: "senderId",
+            as: "Sender",
+        });
 
         this.model.player.hasMany(this.model.message, {
             foreignKey: "playerId",
         });
 
-        this.model.message.belongsTo(this.model.player, {foreignKey: "playerId", as: "Player"});
+        this.model.message.belongsTo(this.model.player, {
+            foreignKey: "playerId",
+            as: "Player",
+        });
 
         this.model.clan.hasMany(this.model.message, {
             foreignKey: "clanId",
         });
-        this.model.message.belongsTo(this.model.clan, {foreignKey: "clanId", as: "Clan"});
+        this.model.message.belongsTo(this.model.clan, {
+            foreignKey: "clanId",
+            as: "Clan",
+        });
 
         /**
-         * connection test then sync the database with the model 
+         * connection test then sync the database with the model
          */
-        this.testConnection()
+        this.testConnection();
     }
 
-    static getInstance(): Database{
-        if (!Database._instance){
+    static getInstance(): Database {
+        if (!Database._instance) {
             Database._instance = new Database();
         }
 
         return Database._instance;
     }
 
-    async testConnection(){
+    async testConnection() {
         try {
             await this._connection.authenticate();
             await this._connection.sync();
@@ -152,26 +188,30 @@ class Database{
     }
 
     async getData(query: string) {
-        let data = await this._connection.query(query, {type: QueryTypes.SELECT });
+        let data = await this._connection.query(query, {
+            type: QueryTypes.SELECT,
+        });
         return data;
     }
 
     async setData(query: string) {
-        let data = await this._connection.query(query, {type: QueryTypes.INSERT });
+        let data = await this._connection.query(query, {
+            type: QueryTypes.INSERT,
+        });
         return data;
     }
 
     async updateData(query: string) {
-        let data = await this._connection.query(query, {type: QueryTypes.UPDATE });
+        let data = await this._connection.query(query, {
+            type: QueryTypes.UPDATE,
+        });
         return data;
     }
 
-    getConnection(): Sequelize | null{
+    getConnection(): Sequelize | null {
         if (this._connection) return this._connection;
-        return null
+        return null;
     }
-
 }
 
-export {Database};
-
+export { Database };
